a65e135274795ac7d88824e8d39c8ded487646d6
Nsundvall760 <bac4mor@gmail.com>
2025-07-21 20:19:45 -0500

    Show trash can delete button for loot routes for admin/mod accounts (always visible)

--- src/pages/GunBuilds.js
+++ src/pages/GunBuilds.js
@@ -788,7 +788,7 @@ function GunBuilds({ isAdmin, adminUsername, onAdminLogout }) {
                             >
                               {route.title}
                             </button>
-                            {isAdmin && adminUsername && adminUsername !== '' && editingSection === 'lootRoutes' && (
+                            {(isAdmin || adminUsername) && (
                               <button
                                 className="absolute top-0 right-0 bg-red-600 text-white rounded-full w-7 h-7 flex items-center justify-center"
                                 onClick={() => handleDeleteLootRoute(route.title)}
